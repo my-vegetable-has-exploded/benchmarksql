8ca636b9699931005c9ef67a266b16fee016beee
my-vegetable-has-exploded <wy1109468038@gmail.com>
2025-05-12 23:23:15 +0800

    feat:use chaosblade for process and disk-fill

---
+++ src/main/java/com/github/pgsqlio/benchmarksql/chaos/BladeClient.java
@@ -0,0 +1,105 @@
+package com.github.pgsqlio.benchmarksql.chaos;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+import org.json.JSONObject;
+
+public class BladeClient {
+
+    public static void main(String[] args) {
+        BladeClient client = new BladeClient();
+        String host = "133.133.135.157";
+        String resultId = client.executeCmd("create cpu load --cpu-percent 50", "133.133.135.157");
+        if (resultId != null) {
+            System.out.println("Command executed successfully, result ID: " + resultId);
+            boolean destroyed = client.destory(resultId, host);
+            if (destroyed) {
+                System.out.println("Successfully destroyed the command with ID: " + resultId);
+            } else {
+                System.out.println("Failed to destroy the command with ID: " + resultId);
+            }
+        } else {
+            System.out.println("Failed to execute command.");
+        }
+    }
+
+    // execute command on remote host using http request
+    public String executeCmd(String cmd, String host) {
+        // # 触发 CPU 负载 50% 场景
+        // curl
+        // "http://xxx.xxx.xxx.xxx:9526/chaosblade?cmd=create%20cpu%20load%20--cpu-percent%2050"
+        // {"code":200,"success":true,"result":"e08a64a9af02c393"}
+        // return result id
+        // execute curl "http://" + host + ":9526/chaosblade?cmd=" + cmd;
+        try {
+            String url = "http://"+host+":9526/chaosblade";
+            String params = "cmd="+cmd;
+
+            // create http request
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .POST(BodyPublishers.ofString(params))
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .build();
+
+            // 3. send request
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+
+            // parse response
+            JSONObject jsonResponse = new JSONObject(response.body());
+            int code = jsonResponse.getInt("code");
+            boolean success = jsonResponse.getBoolean("success");
+            if (success == false) {
+                System.out.println("Error: " + jsonResponse.getString("error"));
+                return null; // return null in case of error
+            }
+            String result = jsonResponse.getString("result");
+
+            // System.out.println("Code: " + code);
+            // System.out.println("Success: " + success);
+            // System.out.println("Result: " + result); // e2b454c4c0ff517d
+            return result; // return the result id
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null; // return null in case of an error
+        }
+    }
+
+    public boolean destory(String id, String host) {
+        // # 销毁实验场景
+        // curl "http://xxx.xxx.xxx.xxx:8080/chaosblade?cmd=destroy%20e08a64a9af02c393"
+        try {
+            String url = "http://"+host+":9526/chaosblade";
+            String params = "cmd=destroy "+id;
+
+            // create http request
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .POST(BodyPublishers.ofString(params))
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .build();
+
+            // 3. send request
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+
+            // parse response
+            JSONObject jsonResponse = new JSONObject(response.body());
+            int code = jsonResponse.getInt("code");
+            boolean success = jsonResponse.getBoolean("success");
+            if (success == false) {
+                System.out.println("Error: " + jsonResponse.getString("error"));
+                return false; // return null in case of error
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false; // return null in case of an error
+        }
+    }
+}
--- src/main/java/com/github/pgsqlio/benchmarksql/chaos/Component.java
+++ src/main/java/com/github/pgsqlio/benchmarksql/chaos/Component.java
@@ -1,22 +1,22 @@
 package com.github.pgsqlio.benchmarksql.chaos;
 
 public class Component {
-    public String ip;
-    public String process;
-    // public String zone;
-    
-    public Component(String component) throws Exception {
-        String[] parts = component.split(":");
-        if (parts.length == 2) {
-            this.ip = parts[0];
-            this.process = parts[1];
-        } else {
-            throw new Exception("Component format error: " + component+ ", should be ip:process like  133.133.135.56:tikv");
-        }
-    }
+  public String host;
+  public String process;
+  // public String zone;
 
-    public String toString() {
-        return ip + ":" + process;
+  public Component(String component) throws Exception {
+    String[] parts = component.split(":");
+    if (parts.length == 2) {
+      this.host = parts[0];
+      this.process = parts[1];
+    } else {
+      throw new Exception("Component format error: " + component + ", should be ip:process like  133.133.135.56:tikv");
     }
-    
+  }
+
+  public String toString() {
+    return host + ":" + process;
+  }
+
 }
